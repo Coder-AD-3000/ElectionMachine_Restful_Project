d6002c0f82415726d33c88df9127045ed996c272
lesheidrich <77836486+lesheidrich@users.noreply.github.com>
2021-04-30 20:43:08 +0300

    question and db update

---
+++ Election_Machine_Restful_Project/src/main/java/client/QuestionClient.java
@@ -0,0 +1,68 @@
+package client;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.client.Invocation.Builder;
+import javax.ws.rs.core.GenericType;
+import data.Question;
+
+
+@WebServlet(urlPatterns = {"/readquestion"})
+public class QuestionClient extends HttpServlet{
+	
+	private static final long serialVersionUID = 1L;
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public QuestionClient() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) 
+	    throws IOException, ServletException {
+		  
+		doGet(request, response);
+	  }
+	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+		throws IOException, ServletException {
+		
+		ArrayList<Question> arrlist = readQuestion(request);
+		
+		request.setAttribute("questionlist", arrlist);
+		RequestDispatcher rd = request.getRequestDispatcher("./jsp/questionnaire.jsp");
+		  rd.forward(request, response);
+	}
+	  
+	
+	private ArrayList<Question> readQuestion(HttpServletRequest request) {
+		
+		String uri = "http://127.0.0.1:8080/rest/questionservice/readQuestion";
+		
+		Client c = ClientBuilder.newClient();
+		WebTarget wt = c.target(uri);
+		Builder b = wt.request();
+		
+		//Create a GenericType to be able to get List of objects
+		//This will be the second parameter of post method
+		GenericType<ArrayList<Question>> gl = new GenericType<ArrayList<Question>>() {};
+		ArrayList<Question> result = b.get(gl);
+		
+		return result;
+	}
+	
+}
---
+++ Election_Machine_Restful_Project/src/main/java/services/QuestionService.java
@@ -0,0 +1,36 @@
+package services;
+
+import java.util.ArrayList;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import data.Question;
+
+
+@Path("/questionservice")
+public class QuestionService {
+
+	EntityManagerFactory emf=Persistence.createEntityManagerFactory("emachinedb");
+	
+	@GET
+	@Path("/readQuestion")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public ArrayList<Question> readQuestion(Question question) {
+			EntityManager em = emf.createEntityManager();
+			em.getTransaction().begin();
+			@SuppressWarnings("unchecked")
+			ArrayList<Question> arrlist = (ArrayList<Question>) em.createQuery("select xyx from Fish xyx").getResultList();		
+			em.getTransaction().commit();
+			
+			System.out.println("Questions read from table..");
+			return arrlist;
+		}
+
+}
