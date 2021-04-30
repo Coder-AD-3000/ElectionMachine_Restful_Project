cf08cc020da9037e3b6eaf344206ade4a7c719e8
Coder-AD-3000 <78084889+Coder-AD-3000@users.noreply.github.com>
2021-04-30 03:05:58 +0300

    Update CandidateService.java

--- Election_Machine_Restful_Project/src/main/java/services/CandidateService.java
+++ Election_Machine_Restful_Project/src/main/java/services/CandidateService.java
@@ -2,6 +2,7 @@ package services;
 
 import java.util.List;
 
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -76,8 +77,6 @@ public class CandidateService {
 		List<Candidate> list=readCandidate();		
 		return list;
 	}	
-	
-		// read to update
 	@GET
 	@Path("/readtoupdatecandidate/{candidate_id}")
 	@Produces(MediaType.APPLICATION_JSON)
