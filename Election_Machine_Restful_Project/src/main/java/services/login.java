f6ebe74721e70c55822a669a6aaea75773eb4deb
lesheidrich <77836486+lesheidrich@users.noreply.github.com>
2021-05-01 19:40:13 +0300

    login

--- Election_Machine_Restful_Project/src/main/java/services/login.java
+++ Election_Machine_Restful_Project/src/main/java/services/login.java
@@ -1,30 +1,45 @@
 package services;
 
+import java.util.ArrayList;
+
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+import data.Candidate;
+
 public class login {
 	
+
+
+	@POST
+	@Path("/candidatelogin/{user}/{pwd}")
+	public String candLogin(@FormParam("user") String user, @FormParam("pwd") String pwd) {
 	
-
-//	  pack class instance "u" with all:
-//		id
-//		username
-//		password
-//from table users..
-
-
-
-//@POST
-//@Path("/user/{user}/{pwd}")
-//public String loginId(@FormParam(user) String user, @FormParam(pwd) String pwd) {
-//
-//ArrayList<User> arrlist = getUsers();//pack all usernames and passwords from table users to arrlist
-//
-//for (User u: arrlist)
-//  if (u.username.equals(user) && u.password.equals(pwd)) {
-//      return u.getId(); //to string
-//      System.out.println("login match with userId: " + u.getId());
-		
-//  }
-//}
+//	ArrayList<Candidate> arrlist = ();//pack all usernames and passwords from table users to arrlist
+	
+	for (Candidate c: arrlist)
+		if (c.username.equals(user) && c.password.equals(pwd)) {
+			return c.getId(); //to string
+			System.out.println("login match with userId: " + c.getId());
+			//start session or add cookie
+	  }
+	}
+	
+	
+	@POST
+	@Path("/employeelogin/{user}/{pwd}")
+	public String empLogin(@FormParam("user") String user, @FormParam("pwd") String pwd) {
+	
+//	ArrayList<Candidate> arrlist = ();//pack all usernames and passwords from table users to arrlist
+	
+	for (Candidate c: arrlist)
+		if (c.username.equals(user) && c.password.equals(pwd)) {
+			return c.getId(); //to string
+			System.out.println("login match with userId: " + c.getId());
+			//start session or add cookie
+	  }
+	}
 
 
 }
