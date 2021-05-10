9a95b80eeada89f7739f06e4e89c730ebfd3e6c6
Daniel Kovacs <kovacsdansan@gmail.com>
2021-05-10 09:35:30 +0200

    Removing unused lines

--- Election_Machine_Restful_Project/src/main/java/services/UploadService.java
+++ Election_Machine_Restful_Project/src/main/java/services/UploadService.java
@@ -6,17 +6,12 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.Date;
-import java.util.List;
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.Query;
 import javax.servlet.ServletContext;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -24,12 +19,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 import org.glassfish.jersey.media.multipart.FormDataParam;
-
-import data.Candidate;
 
 @Path("/uploadservice")
 public class UploadService {
@@ -87,161 +78,5 @@ public class UploadService {
 //	    return Response.temporaryRedirect(location).build();
 //	    return Response.ok().link("/readmyprofile", "wdad").build();
 //	    return Response.ok("Data uploaded successfully !!").build();
-	}
-	
-	@POST
-	@Path("/saveprofileimg")
-	@Consumes({MediaType.MULTIPART_FORM_DATA})
-	public Response saveImage( @FormDataParam("file") InputStream fileInputStream,
-            @FormDataParam("file") FormDataContentDisposition fileMetaData, 
-            @FormDataParam("photo") String photographer,
-            @FormDataParam("camera") String camera,
-            @Context ServletContext sc) 
-            		throws Exception
-	{
-		System.out.println("upload service started");
-		System.out.println("fileMetaData: " + fileMetaData);
-		System.out.println("fileMetaData: " + fileMetaData.getFileName());
-		String UPLOAD_PATH = System.getProperty("user.home") + "/git/ElectionMachine_Restful_Project/Election_Machine_Restful_Project/src/main/webapp/img/";
-	    try{
-	        int read = 0;
-	        byte[] bytes = new byte[1024];
-	 
-	        OutputStream out = new FileOutputStream(new File(UPLOAD_PATH + fileMetaData.getFileName()));
-	        while ((read = fileInputStream.read(bytes)) != -1) 
-	        {
-	            out.write(bytes, 0, read);
-	        }
-	        out.flush();
-	        out.close();
-	        
-	    } 
-	    catch (IOException e){
-	        throw new WebApplicationException("Error while uploading file. Please try again !!");
-	    }
-	    return Response.ok("Data uploaded successfully !!"+
-	    					" Phographer: "+photographer+
-	    					" Camera: "+camera).build();
-	}
-	
-	
-	/*
-	@POST
-	@Path("/fileupload")
-	@Consumes({MediaType.MULTIPART_FORM_DATA})
-	public Response uploadFile( @FormDataParam("file") InputStream fileInputStream,
-            @FormDataParam("file") FormDataContentDisposition fileMetaData, 
-            @FormDataParam("photo") String photographer,
-            @FormDataParam("camera") String camera,
-            @Context ServletContext sc) 
-            		throws Exception
-	{
-		System.out.println("upload service started");
-		System.out.println("fileMetaData: " + fileMetaData);
-		System.out.println("fileMetaData: " + fileMetaData.getFileName());
-//		String UPLOAD_PATH=sc.getRealPath("D:/"); // We dont need getRealPath here!!
-//		String UPLOAD_PATH="D:/";
-		String UPLOAD_PATH = "C:/Users/kovac/git/ElectionMachine_Restful_Project/Election_Machine_Restful_Project/src/main/webapp/img";
-	    try{
-	        int read = 0;
-	        byte[] bytes = new byte[1024];
-	 
-	        OutputStream out = new FileOutputStream(new File(UPLOAD_PATH + "/"+fileMetaData.getFileName()));
-	        while ((read = fileInputStream.read(bytes)) != -1) 
-	        {
-	            out.write(bytes, 0, read);
-	        }
-	        out.flush();
-	        out.close();
-	        
-	    } 
-	    catch (IOException e){
-	        throw new WebApplicationException("Error while uploading file. Please try again !!");
-	    }
-	    return Response.ok("Data uploaded successfully !!"+
-	    					" Phographer: "+photographer+
-	    					" Camera: "+camera).build();
-	}
-	*/
-	
+	}	
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-import java.io.File;
-
-
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
-import javax.servlet.ServletContext;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
-import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
-import org.glassfish.jersey.media.multipart.FormDataParam;
-
-@Path("/uploadservice")
-public class UploadService {
-	@POST
-	@Path("/fileupload")
-	@Consumes({MediaType.MULTIPART_FORM_DATA})
-	public Response uploadFile( @FormDataParam("file") InputStream fileInputStream,
-            @FormDataParam("file") FormDataContentDisposition fileMetaData, 
-            @FormDataParam("photo") String photographer,
-            @FormDataParam("camera") String camera,
-            @Context ServletContext sc) 
-            		throws Exception
-	{
-		
-		String UPLOAD_PATH=sc.getRealPath("C:/Users/kovac/git/ElectionMachine_Restful_Project/Election_Machine_Restful_Project/src/main/webapp/img");
-	    try{
-	        int read = 0;
-	        byte[] bytes = new byte[1024];
-	 
-	        OutputStream out = new FileOutputStream(new File(UPLOAD_PATH + "/"+fileMetaData.getFileName()));
-	        while ((read = fileInputStream.read(bytes)) != -1) 
-	        {
-	            out.write(bytes, 0, read);
-	        }
-	        out.flush();
-	        out.close();
-	        
-	    } 
-	    catch (IOException e){
-	        throw new WebApplicationException("Error while uploading file. Please try again !!");
-	    }
-	    return Response.ok("Data uploaded successfully !!"+
-	    					" Phographer: "+photographer+
-	    					" Camera: "+camera).build();
-	}
-	
-	
-}
-*/
